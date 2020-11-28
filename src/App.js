c2801bb7f3d8ef2688c0fa21ea5869b8a4ad41ab
Nithin <nithin2803@gmail.com>
2020-11-28 13:02:12 +0530

    first map commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
+import { data } from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+export class MapContainer extends React.Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
+  render() {
+    return (
+      <Map onClick={this.onMapClicked} google={this.props.google} initialCenter={{lat: 41.4925, lng: -99.9018 }} zoom={4} style={mapStyles}>
+        {data && data.records.map((place) => {
+          return (
+            <Marker
+            onClick={this.onMarkerClick}
+              key={place.recordid}
+              name={place.fields.team}
+              icon={{
+                url: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/American_football_icon_simple.svg',
+                scaledSize: new this.props.google.maps.Size(25, 25)
+              }}
+              position={{ lat: place.fields.geo_point_2d[0], lng: place.fields.geo_point_2d[1] }}
+            />
+          );
+        })}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCvyC036oaVypkYK47rUpFhYc3dOf2qQ2U",
+})(MapContainer);
---
+++ src/Main.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+};
