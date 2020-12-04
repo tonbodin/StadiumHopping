b516cd178d2974f4185b95996c1df968da4ed2c5
iaden62 <75047037+iaden62@users.noreply.github.com>
2020-12-04 13:03:39 -0800

    Add files via upload

---
+++ src/NFLdata.js
@@ -0,0 +1,226 @@
+export const NFLdata = [
+    {
+        "team": "Arizona Cardinals",
+        "abv": "ARI",
+        "latitude": 33.434,
+        "longitude": -112.008,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/72/Arizona_Cardinals_logo.svg/179px-Arizona_Cardinals_logo.svg.png"
+    },
+    {
+        "team": "Atlanta Falcons",
+        "abv": "ATL",
+        "latitude": 33.64,
+        "longitude": -84.427,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Atlanta_Falcons_logo.svg/192px-Atlanta_Falcons_logo.svg.png"
+    },
+    {
+        "team": "Baltimore Ravens",
+        "abv": "BAL",
+        "latitude": 39.175,
+        "longitude": -76.668,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Baltimore_Ravens_logo.svg/193px-Baltimore_Ravens_logo.svg.png"
+    },
+    {
+        "team": "Buffalo Bills",
+        "abv": "BUF",
+        "latitude": 42.94,
+        "longitude": -78.732,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Buffalo_Bills_logo.svg/189px-Buffalo_Bills_logo.svg.png"
+    },
+    {
+        "team": "Carolina Panthers",
+        "abv": "CAR",
+        "latitude": 35.214,
+        "longitude": -80.943,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Carolina_Panthers_logo.svg/100px-Carolina_Panthers_logo.svg.png"
+    },
+    {
+        "team": "Chicago Bears",
+        "abv": "CHI",
+        "latitude": 41.979,
+        "longitude": -87.904,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Chicago_Bears_logo.svg/100px-Chicago_Bears_logo.svg.png"
+    },
+    {
+        "team": "Cincinnati Bengals",
+        "abv": "CIN",
+        "latitude": 39.046,
+        "longitude": -84.662,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Cincinnati_Bengals_logo.svg/100px-Cincinnati_Bengals_logo.svg.png"
+    },
+    {
+        "team": "Cleveland Browns",
+        "abv": "CLE",
+        "latitude": 41.412,
+        "longitude": -81.85,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/Cleveland_Browns_logo.svg/100px-Cleveland_Browns_logo.svg.png"
+    },
+    {
+        "team": "Dallas Cowboys",
+        "abv": "DAL",
+        "latitude": 32.896,
+        "longitude": -97.037,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Dallas_Cowboys.svg/100px-Dallas_Cowboys.svg.png"
+    },
+    {
+        "team": "Denver Broncos",
+        "abv": "DEN",
+        "latitude": 39.858,
+        "longitude": -104.667,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Denver_Broncos_logo.svg/100px-Denver_Broncos_logo.svg.png"
+    },
+    {
+        "team": "Detroit Lions",
+        "abv": "DET",
+        "latitude": 42.212,
+        "longitude": -83.353,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Detroit_Lions_logo.svg/100px-Detroit_Lions_logo.svg.png"
+    },
+    {
+        "team": "Green Bay Packers",
+        "abv": "GB",
+        "latitude": 44.485,
+        "longitude": -88.129,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Green_Bay_Packers_logo.svg/100px-Green_Bay_Packers_logo.svg.png"
+    },
+    {
+        "team": "Houston Texans",
+        "abv": "HOU",
+        "latitude": 29.98,
+        "longitude": -95.34,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Houston_Texans_logo.svg/100px-Houston_Texans_logo.svg.png"
+    },
+    {
+        "team": "Indianapolis Colts",
+        "abv": "IND",
+        "latitude": 39.717,
+        "longitude": -86.294,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Indianapolis_Colts_logo.svg/100px-Indianapolis_Colts_logo.svg.png"
+    },
+    {
+        "team": "Jacksonville Jaguars",
+        "abv": "JAX",
+        "latitude": 30.494,
+        "longitude": -81.688,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Jacksonville_Jaguars_logo.svg/100px-Jacksonville_Jaguars_logo.svg.png"
+    },
+    {
+        "team": "Kansas City Chiefs",
+        "abv": "KC",
+        "latitude": 39.297,
+        "longitude": -94.714,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e1/Kansas_City_Chiefs_logo.svg/100px-Kansas_City_Chiefs_logo.svg.png"
+    },
+    {
+        "team": "Las Vegas Raiders",
+        "abv": "LV",
+        "latitude": 36.08,
+        "longitude": -115.152,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Las_Vegas_Raiders_logo.svg/100px-Las_Vegas_Raiders_logo.svg.png"
+    },
+    {
+        "team": "Los Angeles Chargers",
+        "abv": "LAC",
+        "latitude": 33.942,
+        "longitude": -118.408,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/7/72/NFL_Chargers_logo.svg/100px-NFL_Chargers_logo.svg.png"
+    },
+    {
+        "team": "Los Angeles Rams",
+        "abv": "LAR",
+        "latitude": 33.942,
+        "longitude": -118.408,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Los_Angeles_Rams_logo.svg/100px-Los_Angeles_Rams_logo.svg.png"
+    },
+    {
+        "team": "Miami Dolphins",
+        "abv": "MIA",
+        "latitude": 25.793,
+        "longitude": -80.291,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en//thumb/3/37/Miami_Dolphins_logo.svg/100px-Miami_Dolphins_logo.svg.png"
+    },
+    {
+        "team": "Minnesota Vikings",
+        "abv": "MIN",
+        "latitude": 44.88,
+        "longitude": -93.217,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Minnesota_Vikings_logo.svg/98px-Minnesota_Vikings_logo.svg.png"
+    },
+    {
+        "team": "New England Patriots",
+        "abv": "NE",
+        "latitude": 41.724,
+        "longitude": -71.428,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/New_England_Patriots_logo.svg/100px-New_England_Patriots_logo.svg.png"
+    },
+    {
+        "team": "New Orleans Saints",
+        "abv": "NO",
+        "latitude": 29.993,
+        "longitude": -90.258,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/New_Orleans_Saints_logo.svg/98px-New_Orleans_Saints_logo.svg.png"
+    },
+    {
+        "team": "New York Giants",
+        "abv": "NYG",
+        "latitude": 40.692,
+        "longitude": -74.169,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/New_York_Giants_logo.svg/100px-New_York_Giants_logo.svg.png"
+    },
+    {
+        "team": "New York Jets",
+        "abv": "NYJ",
+        "latitude": 40.692,
+        "longitude": -74.169,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/New_York_Jets_logo.svg/100px-New_York_Jets_logo.svg.png"
+    },
+    {
+        "team": "Philadelphia Eagles",
+        "abv": "PHI",
+        "latitude": 39.872,
+        "longitude": -75.241,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Philadelphia_Eagles_logo.svg/100px-Philadelphia_Eagles_logo.svg.png"
+    },
+    {
+        "team": "Pittsburgh Steelers",
+        "abv": "PIT",
+        "latitude": 40.491,
+        "longitude": -80.233,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Pittsburgh_Steelers_logo.svg/100px-Pittsburgh_Steelers_logo.svg.png"
+    },
+    {
+        "team": "San Francisco 49ers",
+        "abv": "SF",
+        "latitude": 37.619,
+        "longitude": -122.375,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/San_Francisco_49ers_logo.svg/100px-San_Francisco_49ers_logo.svg.png"
+    },
+    {
+        "team": "Seattle Seahawks",
+        "abv": "SEA",
+        "latitude": 47.449,
+        "longitude": -122.309,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/8/8e/Seattle_Seahawks_logo.svg/100px-Seattle_Seahawks_logo.svg.png"
+    },
+    {
+        "team": "Tampa Bay Buccaneers",
+        "abv": "TB",
+        "latitude": 27.975,
+        "longitude": -82.533,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Tampa_Bay_Buccaneers_logo.svg/100px-Tampa_Bay_Buccaneers_logo.svg.png"
+    },
+    {
+        "team": "Tennessee Titans",
+        "abv": "TEN",
+        "latitude": 36.124,
+        "longitude": -86.678,
+        "team_logo": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/Tennessee_Titans_logo.svg/100px-Tennessee_Titans_logo.svg.png"
+    },
+    {
+        "team": "Washington Football Team",
+        "abv": "WAS",
+        "latitude": 38.852,
+        "longitude": -77.037,
+        "team_logo": "http://www.nflgamedata.com/was.png"
+    }   
+]    
